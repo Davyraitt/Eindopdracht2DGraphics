014f4070168312c6249d36e3402a0f724d75dc34
Davy <dj.raitt@student.avans.nl>
2020-02-14 18:30:49 +0100

    Added a camera class, the camera now also follows the player :)

--- src/Game/Handler.java
+++ src/Game/Handler.java
@@ -2,5 +2,6 @@ package Game;
 
 public class Handler {
 	
-	// The handler class is a simple class that allows us to pass along a bunch of variables when we want to
+	// The handler class is a simple class that allows us to pass along a bunch of variables when we want to.
+	
 }
